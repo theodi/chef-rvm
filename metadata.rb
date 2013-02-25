8fe723d10e14b8b4900249bd0100f04b4d1d1198
pikesley <github@orgraphone.org>
2013-02-25 09:47:28 +0000

    Just Works

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'odi-xml'
+maintainer       'The Open Data Institute'
+maintainer_email 'tech@theodi.org'
+license          'MIT'
+description      'Installs/Configures rvm'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.0.1'
